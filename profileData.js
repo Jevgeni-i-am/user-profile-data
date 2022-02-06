94814b9f85c74299b00a08eb613e79036d0aae47
Jevgeni <67290830+Jevgeni-i-am@users.noreply.github.com>
2022-02-06 15:41:03 +0300

    Add files via upload
    19 profiles with some basic data:
    id number, name, position, image, age, gender, homeCity, phoneNumber.

---
+++ profileData.js
@@ -0,0 +1,194 @@
+export const PD = [
+      {
+            id: 1,
+            name: "Mark Doe",
+            position: "CEO",
+            image: "https://cdn.pixabay.com/photo/2016/01/04/21/57/woman-1121654_960_720.png",
+            age: 18,
+            gender: "male",
+            homeCity: "Tokio",
+            phoneNumber: "+2345789563"
+      },
+
+      {
+            id: 2,
+            name: "Clark Man",
+            position: "Creative designer",
+            image: "https://cdn.pixabay.com/photo/2016/04/26/07/57/woman-1353825_960_720.png",
+            age: 23,
+            gender: "female",
+            homeCity: "New-York",
+            phoneNumber: "+234574545"
+      },
+      {
+            id: 3,
+            name: "Jaden Boor",
+            position: "Front-end dev",
+            image: "https://media.istockphoto.com/vectors/fi/v%C3%A4synyt-nainen-istuu-ja-halaa-polviaan-purkautunut-akku-ajatuksissa-v%C3%A4synyt-nainen-on-id1285425311?k=20&m=1285425311&s=612x612&w=0&h=DvtxOOnE8elWEkcspo8P4gdkcXjPFLuZlCzd1ut37pE=",
+            age: 30,
+            gender: "trans",
+            homeCity: "Moscow",
+            phoneNumber: "+12121212"
+      },
+      {
+            id: 4,
+            name: "Srick Tree",
+            position: "Backend-end dev",
+            image: "https://media.istockphoto.com/vectors/fi/turhautunut-liikemies-vektori-id92580776?k=20&m=92580776&s=612x612&w=0&h=gANtZeNHWC37WrRpuzyhNLCxTyPk-g6_hmtRriiSTOc=",
+            age: 22,
+            gender: "female",
+            homeCity: "SPB",
+            phoneNumber: "+7123123"
+      },
+      {
+            id: 5,
+            name: "John Doe",
+            position: "Creative designer",
+            image: "https://bootdey.com/img/Content/avatar/avatar3.png",
+            age: 25,
+            gender: "female",
+            homeCity: "Helsinki",
+            phoneNumber: "+358454545"
+      },
+      {
+            id: 6,
+            name: "Johanna Doeringo",
+            position: "Manager",
+            image: "https://media.istockphoto.com/vectors/fi/vektori-litte%C3%A4-kuvitus-%C3%A4iti-hell%C3%A4varaisesti-k%C3%A4%C3%A4ritty-vauva-id1135983155?k=20&m=1135983155&s=612x612&w=0&h=jao3IjaU9TRmNuikZqXJGbgfJlC9p9W2N4vUwfsY5tk=",
+            age: 25,
+            gender: "female",
+            homeCity: "London",
+            phoneNumber: "+121213"
+      },
+      {
+            id: 7,
+            name: "Roose De la Garsia",
+            position: "Manager",
+            image: "https://media.istockphoto.com/vectors/fi/vektori-litte%C3%A4-kuvitus-%C3%A4iti-hell%C3%A4varaisesti-k%C3%A4%C3%A4ritty-vauva-id1135983155?k=20&m=1135983155&s=612x612&w=0&h=jao3IjaU9TRmNuikZqXJGbgfJlC9p9W2N4vUwfsY5tk=",
+            age: 35,
+            gender: "female",
+            homeCity: "Madrid",
+            phoneNumber: "+3333333"
+      },
+      {
+            id: 8,
+            name: "Johnathan Fox",
+            position: "IOS dev",
+            image: "https://bootdey.com/img/Content/avatar/avatar1.png",
+            age: 25,
+            gender: "female",
+            homeCity: "Bratislava",
+            phoneNumber: "+65656548"
+      },
+      {
+            id: 9,
+            name: "Mark Rainolds",
+            position: "Web dev",
+            image: "https://media.istockphoto.com/vectors/fi/sarjakuva-naisesta-heiluttamassa-kanadan-p%C3%A4iv%C3%A4n-lippua-id1144282179?k=20&m=1144282179&s=612x612&w=0&h=NMnOVXesdhRUBZos4-1ETCyVTyZvchvxE953Wgbb92c=",
+            age: 33,
+            gender: "male",
+            homeCity: "Kohtla-Järve",
+            phoneNumber: "+37215151515"
+      },
+      {
+            id: 10,
+            name: "Fuck Fucker",
+            position: "Analyst",
+            image: "https://bootdey.com/img/Content/avatar/avatar7.png",
+            age: 69,
+            gender: "male",
+            homeCity: "Amsterdam",
+            phoneNumber: "+69696969"
+      },
+      {
+            id: 11,
+
+            name: "Kale Nyeman",
+            position: "CTO",
+            image: "https://media.istockphoto.com/vectors/fi/nainen-kannettavan-tietokoneen-kanssa-istumassa-luonnossa-id1261467957?k=20&m=1261467957&s=612x612&w=0&h=MMNBKNi8YDQCXxoRiNcWyfT4xbr4yu5m0PrZWk12vks=",
+            age: 45,
+            gender: "male",
+            homeCity: "O'klahoma",
+            phoneNumber: "+123456789"
+      },
+      {
+            id: 12,
+            name: "Jendunio La Ferrera",
+            position: "Analyst",
+            image: "https://media.istockphoto.com/vectors/fi/vektori-sarjakuva-el%C3%A4in-clipart-id1080847354?k=20&m=1080847354&s=612x612&w=0&h=Y4Lw9Wi9mB2a3u5UKiMiaw7aLpzxSufaxzjj-6rLU44=",
+            age: 25,
+            gender: "male",
+            homeCity: "Riga",
+            phoneNumber: "+457856385"
+      },
+      {
+            id: 13,
+            name: "Akuwanbe ahola da'Uoba",
+            position: "Analyst",
+            image: "https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_960_720.png",
+            age: 33,
+            gender: "male",
+            homeCity: "Zimbrabwe",
+            phoneNumber: "+46385"
+      },
+      {
+            id: 14,
+            name: "John Doe Jr.",
+            position: "Analyst",
+            image: "https://cdn.pixabay.com/photo/2020/11/10/15/51/bear-5730216_960_720.png",
+            age: 33,
+            gender: "female",
+            homeCity: "Rome",
+            phoneNumber: "+654987384"
+      },
+      {
+            id: 15,
+            name: "Sam Samon",
+            position: "Analyst",
+            image: "https://bootdey.com/img/Content/avatar/avatar7.png",
+            age: 19,
+            gender: "female",
+            homeCity: "Paris",
+            phoneNumber: "+456212122"
+      },
+      {
+            id: 16,
+            name: "Andrei Hueevech",
+            position: "Analyst",
+            image: "https://media.istockphoto.com/vectors/fi/eristettyn%C3%A4-valkoisesta-naisesta-joka-k%C3%A4rsii-punasilm%C3%A4isuudesta-id1282828821?k=20&m=1282828821&s=612x612&w=0&h=SSU4iETttJCUWu1HPRQv8FFMTPAfTSURN5D1PHif-Q4=",
+            age: 76,
+            gender: "male",
+            homeCity: "Minsk",
+            phoneNumber: "+375753357"
+      },
+      {
+            id: 17,
+            name: "Gari Marteno",
+            position: "Analyst",
+            image: "https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png",
+            age: 41,
+            gender: "male",
+            homeCity: "Tbilisi",
+            phoneNumber: "+375357"
+      },
+      {
+            id: 18,
+            name: "Jeronimo Allrigth",
+            position: "Analyst",
+            image: "https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png",
+            age: 30,
+            gender: "male",
+            homeCity: "Mexico",
+            phoneNumber: "+35555557"
+      },
+      {
+            id: 19,
+            name: "Smart baby",
+            position: "Analyst",
+            image: "https://cdn.pixabay.com/photo/2016/04/26/07/20/woman-1353803_960_720.png",
+            age: 22,
+            gender: "female",
+            homeCity: "Brussele",
+            phoneNumber: "+875421"
+      },
+]
